b0c37ef6f1827c9d4645c45731c31e587c9ebc8c
Marz3587 <marz3587@gmail.com>
2020-03-08 13:49:06 -0400

    added animation (needs work though) as well as some simple walking controls

--- Marc Unity Folder/Assets/Adventure_Kid.cs
+++ Marc Unity Folder/Assets/Adventure_Kid.cs
@@ -4,16 +4,56 @@ using UnityEngine;
 
 public class Adventure_Kid : MonoBehaviour
 {
+    [SerializeField] float STD_MOVE_SPEED = 0.01f;
+    Animation animation;
     // Start is called before the first frame update
     void Start()
     {
-        Animator animator = GetComponent<Animator>();
-        animator.StartPlayback();
+        animation = GetComponent<Animation>();
+        //animation.Play();  // Debug animation playing
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        Vector3 movement = new Vector3(0,0,0);
+        if(Input.GetKey(KeyCode.W))
+        {
+            movement += new Vector3(STD_MOVE_SPEED, 0, 0); //todo Apply rotation
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            movement += new Vector3(-STD_MOVE_SPEED, 0, 0); //todo Apply Rotation
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            movement += new Vector3(0, 0, STD_MOVE_SPEED); //Todo apply rotation
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            movement += new Vector3(0, 0, -STD_MOVE_SPEED); //Todo Apply Rotation
+        }
+
+        if(movement.magnitude > STD_MOVE_SPEED)
+        {
+            movement.Normalize();
+            movement *= STD_MOVE_SPEED;
+        }
+        if ((movement.magnitude > 0) && !(animation.isPlaying))
+        {
+            animation.Play();
+        }
+        else if (movement.magnitude <=0)
+        {
+            animation.Stop();
+        }
+
+        transform.position += movement;
     }
 }
